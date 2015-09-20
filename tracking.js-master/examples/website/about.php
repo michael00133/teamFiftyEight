074e1b389d43e0b5c305da22ae303ee6315f6dde
hopehahn <hope.hahn@gmail.com>
2015-09-20 05:08:54 -0400

    fixed about page, now just waiting for movie

--- tracking.js-master/examples/website/about.php
+++ tracking.js-master/examples/website/about.php
@@ -8,11 +8,13 @@
 <body>
 <?php include "navbar.php"; ?>
 <div class="wrapper">
-<div class="text">
-    <h1 class="heading">Our Team</h1>
-</div>
+<h1 class="heading">Our Team</h1>
 <div class="banner">
     <img class="bannerimg" src="images/TeamPhoto.png" alt="TeamLHHEM">
+</div>
+<div class="banner">
+    <img id="TeamPhoto_nice" src="images/TeamPhoto_nice.jpg" alt="Team Photo Nice">
+    <img id="TeamPhoto_jumping" src="images/TeamPhoto_jumping.jpeg" alt="Team Photo Jumping">
 </div>
 <?php include "footer.php"; ?>
 </body>
