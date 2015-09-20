3a5db97660a753006c61dff73ccd09ddf3a4217d
hopehahn <hope.hahn@gmail.com>
2015-09-20 07:08:21 -0400

    updated

--- tracking.js-master/examples/website/home.php
+++ tracking.js-master/examples/website/home.php
@@ -13,6 +13,7 @@
     <img id="banner_notxt" src="images/banner_noblue.jpg" alt="Banner" />
     <img id="banner_txt" src="images/banner_texts.png" alt="BannerText" />
 </a>
+    
 <script> 
     $(document).ready(function(){
         $("#banner_notxt").hover(function(){
@@ -48,11 +49,22 @@
     </div>
 </div>
 
-<div class="wide">
-    <div class="trace_butt"> </div>
-    <img id="trace_button" src="images/trace_button.png" alt="Start Tracing!"/>
-    <div class="trace_butt"> </div>
+<div class="wide" style="text-align: center">
+    <a href="traces.php"><img style="text-align: center" id="trace_button" src="images/trace_button.png" alt="Start Tracing!"/></a>
 </div>
+    
+<script> 
+    $(document).ready(function(){
+        $("#trace_button").hover(function(){
+            $("trace_button").animate({
+                left: '0',
+                duration: '100000' ,
+                
+            });
+        });
+    });
+</script> 
+    
     
 </div>
     
