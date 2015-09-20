590cd4d6ebbb1a1642a23daf47df83bb6003f13f
hjmoon94 <hm375@cornell.edu>
2015-09-20 07:41:57 -0400

    accuracy check

--- tracking.js-master/examples/website/traces.php
+++ tracking.js-master/examples/website/traces.php
@@ -11,15 +11,8 @@
 
   <style>
 
-    #image {
-      position: relative;
-      margin: auto auto;
-      z-index = 2;
-      display: block;
-      margin-top: 100px;
-    }
-
-    #canvas {
+    #canvas_img,
+    #canvas_draw {
       height: 600px;
       position: absolute;
       width: 800px;
@@ -33,7 +26,7 @@
       z-index = 1;
     }
 
-    canvas, video {
+    video {
       -moz-transform: scale(-1, 1);
       -o-transform: scale(-1, 1);
       -webkit-transform: scale(-1, 1);
@@ -53,39 +46,79 @@
 
     <div style="margin: auto; padding: 15px; height: 600px; width: 800px; background-color: #88d5f1;">
         <video id="video" style="left:0; right:0; margin: 0 auto;" preload autoplay loop muted></video>
-        <canvas id="canvas" style="left:0; right:0; margin: 0 auto;"  width="800" height="600"></canvas>
-       <div style="height: 600px; width: 800px; z-index= 2;"> <img id="image" style="width:300px; height:350px; visibility: hidden;" src="images/0.png"></img></div>
+        <canvas id="canvas_draw" style="left:0; right:0; margin: 0 auto;"  width="800" height="600"></canvas>
+        <canvas id="canvas_img" style="left:0; right:0; margin: 0 auto;"  width="800" height="600"></canvas>
+  <!--  <div style="height: 600px; width: 800px;"> <img id="image" style="width:300px; height:350px; visibility: hidden;" src="images/0.png"></img></div> -->
     </div>
 
     <div style="text-align: center">
         <br><br><br>
         <button id="next-button" onclick="projectLetter()">Let's try writing 0!</button>
+        <p><button id="next-button" onclick="computeScore()">Let's check!</button></p>
     </div>
 
   <script>
     var del = false;
     var imgList = ["0", "1", "2","3","4","5","6","7","8","9"];
     var idx = 0;
+    var image = new Image();
+    var button = document.getElementById('next-button');
 
     var video = document.getElementById('video');
-    var canvas = document.getElementById('canvas');
-    var context = canvas.getContext('2d');
+    var canvas_draw = document.getElementById('canvas_draw');
+    var context_draw = canvas_draw.getContext('2d');
+    var canvas_img = document.getElementById('canvas_img');
+    var context_img = canvas_img.getContext('2d');
 
     var drawSegments = [[]];
     var segment = 0;
-
     var tracker = new tracking.ColorTracker(['red', 'green']);
 
+    var img_data;
+    var draw_data;
+    var img_pixel;
+    var draw_pixel;
+    var diff_pixel;
+
     function projectLetter() {
-        var image = document.getElementById('image');
-        var button = document.getElementById('next-button');
-        image.style.visibility="visible"
-        image.src = "images/"+imgList[idx]+".png";
-        if(idx < 9)
-        	idx++;
-        button.innerHTML = "Let's try writing "+String(idx)+"!";
-        clear();
+      clear(context_img);
+      image.onload = function (){
+        context_img.drawImage(image, 240, 115, 300, 350);
       }
+      image.src = "images/"+imgList[idx]+".png";
+      img_data = context_img.getImageData(0, 0, canvas_img.width, canvas_img.height);
+      img_pixel = img_data.data;
+      img_pixel.map(function(elm){
+        return (elm < 220 ? 0 : 1)
+      });
+      console.log(img_pixel);
+      idx++;
+      button.innerHTML = "Let's try writing "+String(idx)+"!";
+      clear(context_draw);
+    }
+
+    function computeScore() {
+      draw_data = context_draw.getImageData(0, 0, canvas_draw.width, canvas_draw.height);
+      draw_pixel = draw_data.data;
+      draw_pixel.map(function(elm){
+        return (elm < 245 ? 0 : 1)
+      });
+
+      var sum = 0;
+
+      if (idx == 0){
+      }else{
+        for (var j=0, len=draw_pixel.length; j<len; j++){
+          sum += Math.pow((draw_pixel[j]-img_pixel[j]),2);
+        }
+
+        if (sum > 30){
+          console.log("yay");
+        }else{
+          console.log("noo");
+        }
+      }
+    }
 
     tracking.track('#video', tracker, { camera: true });
 
@@ -105,19 +138,19 @@
           erase(rect);
         }
       });
+      
     });
 
     function draw(rect) {
       drawSegments[segment].push(rect.x + rect.width / 2, rect.y + rect.height / 2);
-      console.log(drawSegments);
     }
 
-    function clear(){
-      context.clearRect(0, 0, canvas.width, canvas.height);
+    function clear(context){
+      context.clearRect(0, 0, canvas_draw.width, canvas_draw.height);
     }
 
     function erase(rect) {
-      context.clearRect(rect.x, rect.y, rect.width, rect.height);
+      context_draw.clearRect(rect.x, rect.y, rect.width, rect.height);
     }
 
     function isInsideRect(x, y, rect) {
@@ -127,7 +160,7 @@
 
     (function loop() {
         for (var i = 0, len = drawSegments.length; i < len; i++) {
-            drawSpline(context, drawSegments[i], 0.3, false);
+            drawSpline(context_draw, drawSegments[i], 0.3, false);
         }
 
         drawSegments = [drawSegments[drawSegments.length - 1]];
