218a047167f25fff88a67f36e472ddc4ff009271
hopehahn <hope.hahn@gmail.com>
2015-09-20 02:50:25 -0400

    updated home

--- tracking.js-master/examples/website/home.php
+++ tracking.js-master/examples/website/home.php
@@ -13,7 +13,7 @@
     <img id="banner_notxt" src="images/banner_noblue.jpg" alt="Banner" />
     <img id="banner_txt" src="images/banner_texts.png" alt="BannerText" />
 </a>
-</div>
+
 <script> 
     $(document).ready(function(){
         $("#banner_notxt").hover(function(){
@@ -25,6 +25,17 @@
         });
     });
 </script> 
+<div class="howto">
+    <div id="stepone">
+        <img id="banner_notxt" src="images/HowTo_StepOne.png" alt="HowToStepOne" />
+    
+    </div>
+    
+    
+</div>
+    
+    
+</div>
 <?php include "footer.php"; ?>
 </body>
 </html>
