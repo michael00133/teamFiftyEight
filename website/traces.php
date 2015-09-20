067fd53bba535f146e164c9356d1b46782ea368d
hopehahn <hope.hahn@gmail.com>
2015-09-20 00:31:54 -0400

    updated pages and photos

---
+++ website/traces.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head> 
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <meta charset="utf-8"> 
+    <title>TraceMe</title>
+</head>
+<body>
+<?php include "navbar.php"; ?>
+<div class="wrapper">
+<div class="banner">
+    <img id="bannerimg" src="images/banner_noblue.png" alt="TraceMeHomeBanner">
+    <img id="bannertxt" src="images/logo_orange.png" alt="BannerText">
+</div>
+<div> 
+    <h1>Traces by Popularity</h1>
+</div>
+<div>
+    <h1>Traces by Topic</h1>
+</div>
+<?php include "footer.php"; ?>
+</body>
+</html>
